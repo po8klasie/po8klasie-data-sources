2c0faf18962156af071ade5da1d5ae5c7e1966ef
Michał Oręziak <micorix.dev@gmail.com>
2023-03-13 21:05:39 +0100

    Support extended project configs

--- po8klasie_data_sources/cli/commands/init_projects.py
+++ po8klasie_data_sources/cli/commands/init_projects.py
@@ -1,5 +1,6 @@
 from po8klasie_data_sources.lib.environment_manager import EnvironmentManager
-from po8klasie_fastapi.db.models import Project
+
+from po8klasie_data_sources.project_configs.model_mapper import create_project_model_instance
 
 
 def init_projects(
@@ -7,5 +8,5 @@ def init_projects(
 ):
     with environment_manager.db.session() as session:
         for project_config in environment_manager.config.PROJECT_CONFIGS:
-            session.add(Project(**project_config))
+            session.add(create_project_model_instance(project_config))
         session.commit()
---
+++ po8klasie_data_sources/project_configs/model_mapper.py
@@ -0,0 +1,12 @@
+from po8klasie_fastapi.app.project.models import Project
+from po8klasie_fastapi.app.project.schemas import ProjectSchema
+
+
+def create_project_model_instance(data):
+    validated_data = ProjectSchema.parse_obj(data)
+    return Project(
+        project_id=validated_data.project_id,
+        project_name=validated_data.project_name,
+        school_view_config=validated_data.school_view_config,
+        search_view_config=validated_data.search_view_config
+    )
